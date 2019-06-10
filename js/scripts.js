aeb4ead662f64d5ed4c1383265d12111a4baaa17
willyowi <wilsonowino1@gmail.com>
2019-06-10 11:15:52 +0300

    add form changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,76 @@
 $(document).ready(function() {
   $(".dezin").click(function() {
-    $(".dezin").hide(function(){
-      $(".p1").show(function(){
-        $("#h51").css("font-weight","bold");
+    $(".dezin").hide(function() {
+      $(".p1").show(function() {
+        $("#h51").css("font-weight", "bold");
       });
     });
   });
-  $(".p1").click(function(){
-    $(".dezin").show(function(){
-      $(".p1").hide(function(){
-        $("#h51").css("font-weight","normal");
+  $(".p1").click(function() {
+    $(".dezin").show(function() {
+      $(".p1").hide(function() {
+        $("#h51").css("font-weight", "normal");
       });
     });
   });
 
 
   $(".port").click(function() {
-    $(".port").hide(function(){
-      $(".designs").show(function(){
-        $("#h52").css("font-weight","bold");
+    $(".port").hide(function() {
+      $(".designs").show(function() {
+        $("#h52").css("font-weight", "bold");
       });
     });
   });
-  $(".designs").click(function(){
-    $(".port").show(function(){
-      $(".designs").hide(function(){
-        $("#h52").css("font-weight","normal");
+  $(".designs").click(function() {
+    $(".port").show(function() {
+      $(".designs").hide(function() {
+        $("#h52").css("font-weight", "normal");
       });
     });
   });
 
   $(".prod").click(function() {
-    $(".prod").hide(function(){
-      $(".designz").show(function(){
-          $("#h53").css("font-weight","bold");
+    $(".prod").hide(function() {
+      $(".designz").show(function() {
+        $("#h53").css("font-weight", "bold");
       });
     });
   });
-  $(".designz").click(function(){
-    $(".prod").show(function(){
-      $(".designz").hide(function(){
-          $("#h53").css("font-weight","normal");
+  $(".designz").click(function() {
+    $(".prod").show(function() {
+      $(".designz").hide(function() {
+        $("#h53").css("font-weight", "normal");
       });
     });
   });
 
-});
-//******************portfolio*****************
-$(document).ready(function(){
-  $(".p-image").hover(function(){
-    $(".overlay").css("background-color","white")
+
+
+  $('#feedback').submit(function(event) {
+    var name = $('input#name').val();
+    var email = $('input#email').val();
+    if (name && email != '') {
+      alert('Thank you so much for contacting us  ' + name + ' we appreciate your feedback');
+      alert("Your message has been received!")
+    }
+     else {
+      alert('Invalid input');
+    }
+    event.preventDefault();
   });
+
+
+  $(".p-image").hover(function() {
+    $(".overlay").css("background-color", "white")
+  });
+
+
+
+
+
+
+
+
+
 });
